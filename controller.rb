63d8be5066ebcd5256c3b5c7f9bf73ce51da11b1
David Rawson <dm.rawson@gmail.com>
2017-12-12 20:37:13 +0000

    Welcome page with rules added, and a wee bit of css.

--- controller.rb
+++ controller.rb
@@ -6,6 +6,13 @@ require ('pry-byebug')
 
 require_relative ('./models/rock_paper_scissors.rb')
 
+get ('/') do
+  erb( :home )
+end
+
+get ('/result') do
+  erb( :result)
+end
 
 get ('/:hand1/:hand2') do
   game = RockPaperScissors.new(params[:hand1], params[:hand2])
